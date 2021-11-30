052d213488f9db5c9de04b296764d5d20bb23ab6
Md Arifur Rahman <rahman1193arif@gmail.com>
2021-11-30 20:13:41 +0600

    finisg

--- src/Components/ContractMe/ContractMe.jsx
+++ src/Components/ContractMe/ContractMe.jsx
@@ -66,7 +66,7 @@ const ContractMe = () => {
                 <div className="c-right">
                     <p className="c-desc">
                         <b>What’s your story?</b> Get in touch. Always available for
-                        freelancing if the right project comes along. me.
+                        freelancing if the right project comes along me...
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
                         <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="from_name" />
