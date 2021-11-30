4c218aec8501c6d6669104f14ac9ce839fc10435
Md Arifur Rahman <rahman1193arif@gmail.com>
2021-11-30 17:13:21 +0600

    resume

--- src/Components/ProjectList/ProjectList.jsx
+++ src/Components/ProjectList/ProjectList.jsx
@@ -21,26 +21,7 @@ const ProjectList = () => {
           ))}
         </div>
       </div>
-        // <div className="project-list">
-        //     <div className="project-list-texts">
-        //         <h1 className="project-list-title">Create & inspire. It's Lama</h1>
-        //         <p className="project-list-desc">
-        //             Lama is a creative portfolio that your work has been waiting for.
-        //             Beautiful homes, stunning portfolio styles & a whole lot more awaits
-        //             inside.
-        //         </p>
-        //     </div>
-        //     <div className="project-list">
-        //         {products.map((item)=>(
-        //             <Projects
-        //             key = {item.id}
-        //              img={item.img} 
-        //              link={item.link}  
-        //             ></Projects>
-        //         ))}
-            
-        //     </div>
-        // </div>
+       
     )
 }
 
